b497dc9713a8190a6df0b7438f7dba6b06c35140
叶大放 <fangbig@fangdeMacBook-Pro.local>
2017-06-05 15:22:10 +0800

    done slide

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+
+    <router-view name="viewA"></router-view>
+    <router-view name="viewB"></router-view>
+    <router-view ></router-view>
+
+    <router-link :to="{name:'foo1'}">go to foo</router-link>
+    <router-link to="/bar">go to bar</router-link>
+    <button @click="getParams">getParams</button>
+  </div>
+</template>
+
+<script>
+// import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {},
+  methods:{
+      getParams(){
+          console.log(this.$route.params);
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import Layout from './components/layout'
+import IndexPage from './pages/index'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+
+let router = new VueRouter({
+    mode:'history',
+    routes:[
+        {
+            path:'/',
+            component: IndexPage
+        }
+    ]
+})
+
+new Vue({
+    el:'#app',
+    router,
+    template:'<Layout/>',
+    components:{ Layout }
+})
